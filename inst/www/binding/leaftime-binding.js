559af565c45d26c0811f185d97359d2abfa7f419
timelyportfolio <kent.russell@timelyportfolio.com>
2019-07-28 12:55:01 -0500

    experiment adding timeline to layer manager

--- inst/www/binding/leaftime-binding.js
+++ inst/www/binding/leaftime-binding.js
@@ -11,5 +11,8 @@ LeafletWidget.methods.addTimeline = function(group, data, timelineOptions, slide
     if(typeof(onchange) !== 'undefined' && onchange !== null) {
       timeline.on('change', onchange);
     }
+
+    // not the official way for layerManager but we'll hack for now
+    map.layerManager._byCategory.timeline = L.layerGroup([timelineControl, timeline]);
   }).call(this);
 };
